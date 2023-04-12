7724f69a8c7c099b1e78fa6c68a323dd9fedf61d
PyBeginner1 <shashvathn@gmail.com>
2023-04-12 18:11:50 +0530

    Logging testing completed

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from housing.logger import logging
+
+
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    logging.info('Testing')
     return 'Hello world'
 
 
