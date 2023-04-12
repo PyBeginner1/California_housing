879d919c74a47ba761dac1b027d60349e99264ff
PyBeginner1 <shashvathn@gmail.com>
2023-04-12 18:11:22 +0530

    Logging done

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -7,7 +7,7 @@ LOG_DIR="housing_logs"
 CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 LOG_FILE_NAME = f"log_{CURRENT_TIME_STAMP}.log"
 
-os.make_dirs(LOG_DIR, exist_ok=True)
+os.makedirs(LOG_DIR, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
 
