3712248547ea0359bacf3ac30f8e2a441ae9c9af
PyBeginner1 <shashvathn@gmail.com>
2023-04-14 11:33:52 +0530

    Updated

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -10,7 +10,7 @@ CONFIG_FILE_PATH=os.path.join(ROOT_DIR,CONFIG_DIR,CONFIG_FILE)
 
 
 def get_current_time_stamp():
-    return f"datetime.now().strftime('%Y-%m-%d_%H-%M-%S')"
+    return f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 
 CURRENT_TIME_STAMP = get_current_time_stamp()
 
