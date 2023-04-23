165beb6f324216d9a528c4a942bc75bb2194137d
PyBeginner1 <shashvathn@gmail.com>
2023-04-23 08:09:53 +0530

    Model Evaluation stage completed

--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -1,14 +1,15 @@
 import sys
 
 from housing.config.configuration import Configuration
-from housing.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact,DataTransformationArtifact
-from housing.entity.config_entity import DataIngestionConfig,DataValidationConfig,ModelTrainerConfig
+from housing.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact,DataTransformationArtifact,ModelTrainerArtifact,ModelEvaluationArtifact
+from housing.entity.config_entity import DataIngestionConfig,DataValidationConfig,ModelTrainerConfig,ModelEvaluationConfig
 from housing.logger import logging
 from housing.exception import HousingException
 from housing.component.data_ingestion import DataIngestion
 from housing.component.data_validation import DataValidation
 from housing.component.data_transformation import DataTransformation
 from housing.component.model_trainer import ModelTrainer
+from housing.component.model_evaluation import ModelEvaluation
 
 class Pipeline:
     def __init__(self, config: Configuration):
@@ -53,11 +54,18 @@ class Pipeline:
             raise HousingException(e,sys) from e
         
     
-    def start_model_evaluation(self):
+    def start_model_evaluation(self, data_ingestion_artifact: DataIngestionArtifact,
+                               data_validation_artifact: DataValidationArtifact,
+                               model_trainer_artifact: ModelTrainerArtifact) -> ModelEvaluationArtifact:
         try:
-            pass
+            model_eval = ModelEvaluation(
+                model_evaluation_config=self.config.get_model_evaluation_config(),
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_artifact=data_validation_artifact,
+                model_trainer_artifact=model_trainer_artifact)
+            return model_eval.initiate_model_evaluation()
         except Exception as e:
-            raise HousingException(e,sys) from e
+            raise HousingException(e, sys) from e
         
 
     def start_model_pusher(self):
@@ -76,5 +84,8 @@ class Pipeline:
                                            data_validation_artifact=data_validation_artifact)
             model_trainer_artifact=self.start_model_trainer(model_trainer_config=self.config.get_model_trainer_config(),
                                                             data_transformation_artifact=data_transformation_artifact)
+            model_evaluation_artifact=self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
+                                                                    data_validation_artifact=data_validation_artifact,
+                                                                    model_trainer_artifact=model_trainer_artifact)
         except Exception as e:
             raise HousingException(e,sys) from e
